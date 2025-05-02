878c147b29d7a458acaa237cff3c104dab61c242
eleadn <eleano.dn@gmail.com>
2025-05-02 21:39:03 +0200

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports =
 {
@@ -10,4 +11,11 @@ module.exports =
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    plugins:
+    [
+        new HtmlWebpackPlugin(
+        {
+            template: "./src/template.html",
+        }),
+    ],
 }
